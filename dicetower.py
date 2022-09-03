1b426e1d1c56aa5411832a8c55afcb473d0ef624
Julian Gruber <julian@juliangruber.com>
2022-09-03 10:10:01 +0200

    swap num and type variables

--- dicetower.py
+++ dicetower.py
@@ -1,41 +1,41 @@
 import random
 
-def dice_tower(num = int(input("How many sides do your dice have? \n> "))):
+def dice_tower(type = int(input("How many sides do your dice have? \n> "))):
 	i = 0
 	end = []
-	if num in [4, 6, 8, 10, 12, 20, 100]:
-		type = int(input("How many dice do you want to roll? \n> "))
-		if num == 4:
-			while i<type:
+	if type in [4, 6, 8, 10, 12, 20, 100]:
+		num = int(input("How many dice do you want to roll? \n> "))
+		if type == 4:
+			while i<num:
 				end.append(random.randint(1, 4))
 				i += 1
-		elif num == 6:
-			while i<type:
+		elif type == 6:
+			while i<num:
 				end.append(random.randint(1, 6))
 				i += 1
-		elif num == 8:
-			while i<type:
+		elif type == 8:
+			while i<num:
 				end.append(random.randint(1, 8))
 				i += 1
-		elif num == 10:
-			while i<type:
+		elif type == 10:
+			while i<num:
 				end.append(random.randint(1, 10))
 				i += 1
-		elif num == 12:
-			while i<type:
+		elif type == 12:
+			while i<num:
 				end.append(random.randint(1, 12))
 				i += 1
-		elif num == 20:
-			while i<type:
+		elif type == 20:
+			while i<num:
 				end.append(random.randint(1, 20))
 				i += 1
-		elif num == 100:
-			while i<type:
+		elif type == 100:
+			while i<num:
 				end.append(random.randint(1, 100))
 				i += 1
 	else:
 		print("Your dice does not exist! Restart!")
-		return dice_tower(num = int(input("How many sides do your dice have? \n> ")))
+		return dice_tower(type = int(input("How many sides do your dice have? \n> ")))
 
 	print(end)
 
